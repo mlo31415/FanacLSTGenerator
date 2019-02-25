00d65e88001acd072777a2e87c05c86aaf4bffb5
Mark Olson <mlo@baskerville.org>
2019-02-24 21:33:43 -0500

    Convert to using wxFormBuilder

--- FanacLSTGenerator.py
+++ FanacLSTGenerator.py
@@ -1,26 +1,12 @@
 import os
 import wx
 import wx.grid
+from GUIClass import GUIClass
 from LSTFile import *
 
-class MainWindow(wx.Frame):
+class MainWindow(GUIClass):
     def __init__(self, parent, title):
-        wx.Frame.__init__(self, parent, title=title, size=(600,600))
-        #self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
-        self.statusbar=self.CreateStatusBar() # A Statusbar in the bottom of the window
-
-        # Setting up the menu.
-        filemenu=wx.Menu()
-        # wx.ID_ABOUT and wx.ID_EXIT are standard IDs provided by wxWidgets.
-        filemenu.Append(wx.ID_ABOUT, "&About", " Information about this program")
-        filemenu.AppendSeparator()
-        filemenu.Append(wx.ID_EXIT, "E&xit", " Terminate the program")
-
-        # Creating the menubar.
-        menuBar=wx.MenuBar()
-        menuBar.Append(filemenu, "&File")  # Adding the "filemenu" to the MenuBar
-        self.SetMenuBar(menuBar)  # Adding the MenuBar to the Frame content.
-        panel=wx.Panel(self)
+        GUIClass.__init__(self, parent)
 
         # Call the File Open dialog to get an LST file
         self.dirname=''
@@ -36,17 +22,12 @@ class MainWindow(wx.Frame):
         self.lstData=ReadLstFile(self.lstFilename)
 
         # Create a wxGrid object
-        self.grid=grid=wx.grid.Grid()
-        # Call CreateGrid to set the dimensions of the grid
-        grid.Create(panel)
-        grid.CreateGrid(1, 1+len(self.lstData.ColumnHeaders))
-        grid.SetDefaultColSize(50, True)
-        grid.SetDefaultRowSize(20, True)
+        grid=self.gRowGrid
 
-        grid.HideRowLabels()
-        #grid.EnableGridLines(False)
+        # grid.HideRowLabels()
 
         # Add the column headers
+        grid.SetColLabelValue(0, "First Page")
         i=1
         for colhead in self.lstData.ColumnHeaders:
             grid.SetColLabelValue(i, colhead)
@@ -71,39 +52,12 @@ class MainWindow(wx.Frame):
         grid.AutoSizeColumns()
 
         # Let's lay out the space.  We fill the panel with a vertical sizer so things in it are stacked vertically.
-        # Inside that we have a top sizer for small controls and a second sizer below it for the grid.
+        # Inside that we have a top sizer for small controls and a second sizer below it for the gRowGrid.
         gbs=wx.GridBagSizer(4,2)
-
-        # The top gridbag row gets buttons
-        self.buttonLoad=wx.Button(panel, id=wx.ID_ANY, label="Save LST file")
-        gbs.Add(self.buttonLoad, (0,0))
-        self.buttonLoad.Bind(wx.EVT_BUTTON, self.OnSaveLSTButtonClicked)
-
-        self.buttonGenerate=wx.Button(panel, id=wx.ID_ANY, label="Rename")
-        gbs.Add(self.buttonGenerate, (0,1))
-
-        # Now put a pair of buttons with labels above them in the middle two gridbag rows
-        gbs.Add(wx.StaticText(panel, label=" Fanzine name"), (1,0))
-        self.fanzineNameTextbox=wx.TextCtrl(panel, id=wx.ID_ANY)
-        gbs.Add(self.fanzineNameTextbox, (2,0))
-        self.fanzineNameTextbox.Bind(wx.EVT_TEXT, self.OnFanzinenameOrIssueTextboxChanged)
-
-        gbs.Add(wx.StaticText(panel, label=" Issue number"), (1,1))
-        self.fanzineIssuenumber=wx.TextCtrl(panel, id=wx.ID_ANY)
-        gbs.Add(self.fanzineIssuenumber, (2,1))
-        self.fanzineIssuenumber.Bind(wx.EVT_TEXT, self.OnFanzinenameOrIssueTextboxChanged)
-
-        self.grid.Bind(wx.grid.EVT_GRID_CELL_CHANGED, self.OnGridCellChanged)
-
-        # And the grid itself goes in the bottom gridbag row, spanning both columns
-        gbs.Add(grid, (3,0), span=(2,2))
-
-        panel.SetSizerAndFit(gbs)
 
         self.Show(True)
 
-
-    def OnSaveLSTButtonClicked(self, event):
+    def OnSaveLSTFile(self, event):
         if event.EventObject.Label != "Save LST file":
             return
         content=[]
@@ -121,24 +75,26 @@ class MainWindow(wx.Frame):
         with open(newname, "w+") as f:
             f.writelines([c+"\n" for c in content])
 
+    def OnLoadNewIssues(self, event):
+        pass
 
-    def OnGridCellChanged(self, evt):
-        row=evt.GetRow()
-        col=evt.GetCol()
+    def OnGridCellKeyUp(self, event):
+        i=0
+
+    def OnTextTopMatter(self, event):
+        pass
+
+    def OnTextComments(self, event):
+        pass
+
+    def OnGridCellChanged(self, event):
+        row=event.GetRow()
+        col=event.GetCol()
         if col > 1:
-            self.lstData.Rows[row][col-1]=self.grid.GetCellValue(row, col)
+            self.lstData.Rows[row][col-1]=self.gRowGrid.GetCellValue(row, col)
         else:
-            self.lstData.Rows[row][0]=self.grid.GetCellValue(row, 0)+">"+self.grid.GetCellValue(row, 1)
+            self.lstData.Rows[row][0]=self.gRowGrid.GetCellValue(row, 0)+">"+self.gRowGrid.GetCellValue(row, 1)
 
-
-    def UpdateNewFilenames(self):
-        for i in range(0, len(self.pageNum)):
-            name=self.fanzineNameTextbox.Value+self.fanzineIssuenumber.Value+self.grid.GetCellValue(i,1)
-            self.grid.SetCellValue(i, 2, name+".jpg")
-            self.grid.SetCellBackgroundColour(i, 2, wx.Colour(255,230,230) if len(name) > 8 else wx.Colour(255,255,255))
-
-    def OnFanzinenameOrIssueTextboxChanged(self, event):
-        self.UpdateNewFilenames()
 
 app = wx.App(False)
 frame = MainWindow(None, "Sample editor")
---
+++ GUIClass.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+
+###########################################################################
+## Python code generated with wxFormBuilder (version Jun 17 2015)
+## http://www.wxformbuilder.org/
+##
+## PLEASE DO "NOT" EDIT THIS FILE!
+###########################################################################
+
+import wx
+import wx.xrc
+import wx.grid
+
+
+###########################################################################
+## Class MainFrame
+###########################################################################
+
+class GUIClass(wx.Frame):
+
+    def __init__(self, parent):
+        wx.Frame.__init__(self, parent, id=wx.ID_ANY, title=wx.EmptyString, pos=wx.DefaultPosition, size=wx.Size(1000, 700), style=wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL)
+
+        self.SetSizeHintsSz(wx.DefaultSize, wx.DefaultSize)
+
+        self.m_toolBar2=self.CreateToolBar(wx.TB_HORIZONTAL, wx.ID_ANY)
+        self.bSaveLSTFile=wx.Button(self.m_toolBar2, wx.ID_ANY, u"Save LST File", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.m_toolBar2.AddControl(self.bSaveLSTFile)
+        self.mLoadNewIssues=wx.Button(self.m_toolBar2, wx.ID_ANY, u"Load New Issue(s)", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.m_toolBar2.AddControl(self.mLoadNewIssues)
+        self.m_toolBar2.Realize()
+
+        bSizer1=wx.BoxSizer(wx.VERTICAL)
+
+        fgSizer2=wx.FlexGridSizer(2, 2, 0, 0)
+        fgSizer2.AddGrowableCol(1)
+        fgSizer2.AddGrowableRow(1)
+        fgSizer2.SetFlexibleDirection(wx.BOTH)
+        fgSizer2.SetNonFlexibleGrowMode(wx.FLEX_GROWMODE_SPECIFIED)
+
+        self.m_staticText1=wx.StaticText(self, wx.ID_ANY, u"Top matter:", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.m_staticText1.Wrap(-1)
+        fgSizer2.Add(self.m_staticText1, 0, wx.ALL, 5)
+
+        self.tTopMatter=wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0)
+        fgSizer2.Add(self.tTopMatter, 0, wx.ALL|wx.EXPAND, 5)
+
+        self.m_staticText2=wx.StaticText(self, wx.ID_ANY, u"<P>Comments</P>:", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.m_staticText2.Wrap(-1)
+        fgSizer2.Add(self.m_staticText2, 0, wx.ALL, 5)
+
+        self.tPText=wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0)
+        fgSizer2.Add(self.tPText, 0, wx.ALL|wx.EXPAND, 5)
+
+        bSizer1.Add(fgSizer2, 1, wx.ALL|wx.EXPAND, 5)
+
+        theIssueGrid=wx.BoxSizer(wx.VERTICAL)
+
+        self.gRowGrid=wx.grid.Grid(self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, 0)
+
+        # Grid
+        self.gRowGrid.CreateGrid(100, 10)
+        self.gRowGrid.EnableEditing(True)
+        self.gRowGrid.EnableGridLines(True)
+        self.gRowGrid.EnableDragGridSize(False)
+        self.gRowGrid.SetMargins(0, 0)
+
+        # Columns
+        self.gRowGrid.AutoSizeColumns()
+        self.gRowGrid.EnableDragColMove(True)
+        self.gRowGrid.EnableDragColSize(True)
+        self.gRowGrid.SetColLabelSize(30)
+        self.gRowGrid.SetColLabelAlignment(wx.ALIGN_CENTRE, wx.ALIGN_CENTRE)
+
+        # Rows
+        self.gRowGrid.AutoSizeRows()
+        self.gRowGrid.EnableDragRowSize(True)
+        self.gRowGrid.SetRowLabelSize(80)
+        self.gRowGrid.SetRowLabelAlignment(wx.ALIGN_CENTRE, wx.ALIGN_CENTRE)
+
+        # Label Appearance
+
+        # Cell Defaults
+        self.gRowGrid.SetDefaultCellAlignment(wx.ALIGN_LEFT, wx.ALIGN_TOP)
+        theIssueGrid.Add(self.gRowGrid, 0, wx.ALL|wx.EXPAND, 5)
+
+        bSizer1.Add(theIssueGrid, 1, wx.EXPAND, 5)
+
+        self.SetSizer(bSizer1)
+        self.Layout()
+
+        self.Centre(wx.BOTH)
+
+        # Connect Events
+        self.bSaveLSTFile.Bind(wx.EVT_BUTTON, self.OnSaveLSTFile)
+        self.mLoadNewIssues.Bind(wx.EVT_BUTTON, self.OnLoadNewIssues)
+        self.tTopMatter.Bind(wx.EVT_TEXT, self.OnTextTopMatter)
+        self.tPText.Bind(wx.EVT_TEXT, self.OnTextComments)
+        self.gRowGrid.Bind(wx.grid.EVT_GRID_CELL_CHANGED, self.OnGridCellChanged)
+        self.gRowGrid.Bind(wx.EVT_KEY_UP, self.OnGridCellKeyUp)
+
+    def __del__(self):
+        pass
+
+    # Virtual event handlers, overide them in your derived class
+    def OnSaveLSTFile(self, event):
+        event.Skip()
+
+    def OnLoadNewIssues(self, event):
+        event.Skip()
+
+    def OnTextTopMatter(self, event):
+        event.Skip()
+
+    def OnTextComments(self, event):
+        event.Skip()
+
+    def OnGridCellChanged(self, event):
+        event.Skip()
+
+    def OnGridCellKeyUp(self, event):
+        event.Skip()
+
+
